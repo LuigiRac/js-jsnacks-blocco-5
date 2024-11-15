e13cc77f133c5f3285c591553bea94ae4a897423
LuigiRac <lui.rac06@gmail.com>
2024-11-15 16:57:27 +0100

    8 snack

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classeStudente = students.find((classe) => {
+  return classe.id == 1;
+});
+
+console.log(classeStudente.class);
