239ca2dd5e16074c250505fbf777dc4cb3dc7a3b
LuigiRac <lui.rac06@gmail.com>
2024-11-15 15:51:48 +0100

    4 snack

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorName = posts.map((firstName) => {
+  return firstName.author;
+});
+console.log(authorName);
